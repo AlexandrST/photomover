e8070745e49fe0abadcdb3322a4210a8cf7a74b9
miraage <miraage.wow@gmail.com>
2015-07-26 01:10:45 +0300

    refactor(app): Mikhail loves to refactor

--- photomover.php
+++ photomover.php
@@ -6,12 +6,3 @@ if (PHP_SAPI !== 'cli') {
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
-
-use Photomover\Application;
-use Photomover\Environment;
-
-$env = new Environment(__DIR__);
-$env->init();
-
-$app = new Application();
-exit($app->run());
